a58835d12ceadbb59df7416c846ccff4721a255d
Jonathan Haile <wonafan@gmail.com>
2016-01-05 23:24:13 -0800

    aws issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
     :storage => :s3,
     :s3_credentials => {
       :s3_region => 'us-west-1',
-      :bucket => ENV['S3_BUCKET_NAME'],
+      :bucket => ENV['S3_wonafan_pintsersting'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
