1503f238fb3c2dac3e6af44714c47f3afc152280
Jonathan Haile <wonafan@gmail.com>
2016-01-09 22:10:34 -0800

    added names to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :pins       
+  has_many :pins
+
+  validates :name, presence: true       
 end
